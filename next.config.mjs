3ceb13d2503637d01750db1ee6f476f5f188c186
alexdracup1-ux <alexdracup1@gmail.com>
2025-10-08 16:52:29 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  trailingSlash: true,  // Optional: Makes URLs like /oferty/ (helps with WordPress routing)
+  images: {
+    unoptimized: true,  // Required for static export; skips Next.js image optimization
+  },
 };
 
 export default nextConfig;
